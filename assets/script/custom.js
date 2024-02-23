723359699a1641df1537111fd6b3585979751072
shehzad <shehzadamin697@gmail.com>
2024-02-23 21:20:25 +0500

    upadted the article page

--- assets/script/custom.js
+++ assets/script/custom.js
@@ -25,6 +25,19 @@ jQuery(document).ready(function($){
     $('#myCarousel1').carousel({
         pause: 'hover'
     });
+
+    // 
+    // cat dropdown\
+    $('.cat_dropdown').on('click', function(e){
+        e.preventDefault();
+        $('.cat_dropdown').removeClass('is_checked');
+        $(this).toggleClass('is_checked');
+    });
+    $('.cat_dropdown a').on('click', function(e){
+        e.preventDefault();
+        $('.cat_dropdown a').removeClass('is_checked');
+        $(this).toggleClass('is_checked');
+    });
 });
 
 
@@ -34,26 +47,26 @@ jQuery(document).ready(function($){
 
 
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-36251023-1']);
-_gaq.push(['_setDomainName', 'jqueryscript.net']);
-_gaq.push(['_trackPageview']);
+// var _gaq = _gaq || [];
+// _gaq.push(['_setAccount', 'UA-36251023-1']);
+// _gaq.push(['_setDomainName', 'jqueryscript.net']);
+// _gaq.push(['_trackPageview']);
 
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+// (function() {
+//   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+//   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+//   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+// })();
 
-try {
-fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' })).then(function(response) {
-  return true;
-}).catch(function(e) {
-  var carbonScript = document.createElement("script");
-  carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CK7DKKQU&placement=wwwjqueryscriptnet";
-  carbonScript.id = "_carbonads_js";
-  document.getElementById("carbon-block").appendChild(carbonScript);
-});
-} catch (error) {
-console.log(error);
-}
+// try {
+// fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' })).then(function(response) {
+//   return true;
+// }).catch(function(e) {
+//   var carbonScript = document.createElement("script");
+//   carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CK7DKKQU&placement=wwwjqueryscriptnet";
+//   carbonScript.id = "_carbonads_js";
+//   document.getElementById("carbon-block").appendChild(carbonScript);
+// });
+// } catch (error) {
+// console.log(error);
+// }
